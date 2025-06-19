31c81e7a6f4af6a7cf0a620e466a1d3319c73696
Etno Polino <einshoch66@gmail.com>
2025-06-20 00:24:10 +0200

    1- Setting our own username and password

---
+++ src/test/java/com/etnopolino/project/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etnopolino.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
