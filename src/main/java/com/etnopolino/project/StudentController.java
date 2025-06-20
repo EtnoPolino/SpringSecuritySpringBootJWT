3a785e30cf12b7eba54ecb0e8d6c20c5a86565da
Etno Polino <einshoch66@gmail.com>
2025-06-20 23:28:41 +0200

    CSRF Token
    Cross Site Request Forgery.

---
+++ src/main/java/com/etnopolino/project/StudentController.java
@@ -0,0 +1,35 @@
+package com.etnopolino.project;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    public List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Etno", 10),
+            new Student(2, "Gorgious", 20)
+    ));
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+}
